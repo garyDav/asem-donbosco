5b434b664c78a5d680a610f91187e30e53a9522a
garyDav <gary.2810.dav@gmail.com>
2022-10-26 20:45:11 -0400

    first commit

---
+++ src/HomeApp.jsx
@@ -0,0 +1,12 @@
+import { Provider } from 'react-redux'
+
+import { store } from './store/store'
+import { AppRouter } from './routers/AppRouter'
+
+export const HomeApp = () => {
+  return (
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import { HomeApp } from './HomeApp'
+import './styles/styles.scss'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HomeApp />
+  </React.StrictMode>
+)
